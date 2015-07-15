e9abb909321cb1f4c938ff47a94b77260b7a8dcb
Johnathan Craig (jcraig4803) <Jcraig4803@users.noreply.github.com>
2015-07-14 21:10:26 -0400

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,9 @@
+ function Makebold(e)
+    {
+       $(e).css("font-weight", "bold")
+    }
+      function Makelight(e)
+    {
+        $(e).css("font-weight", "normal")
+    }
+$("body *").css("font-weight", "bold")
